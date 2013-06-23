e591d6da5764b4aafe85737a9a777daaefc83879
kawahara <solcreate@gmail.com>
2013-06-23 19:49:17 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Circles::Application.config.secret_token = '8d81f980aa1daa9051b1a7e8d11aa22c2055143f236296d4114d3c8a33a8867f20982f08d6548b420b0ecec8201dc10dc1f45e167eebc67b6b556066612c39a4'
